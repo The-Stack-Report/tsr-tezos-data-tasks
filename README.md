985f1909f75b82728b73d8c7d2aa980c8000fe9d
Erwin Hoogerwoord <erwin@dialectic.design>
2023-05-01 12:24:30 +0200

    added requirements and task that reads sql into a pandas dataframe

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Repository that contains a number of tasks for processing Tezos blockchain data
 
 This code is meant to be deployed within the context of the Stack Report Data Services infra.
+
+Within the Task Worker module, the `tasks` and `task_utils` directories will be copied over as python modules.
+
--- tasks/tezos_accounts_index/tezos_accounts_index.py
+++ tasks/tezos_accounts_index/tezos_accounts_index.py
@@ -1,6 +1,18 @@
-from task_utils.pg_db import connection
+from task_utils.pg_db import dbConnection
+import pandas as pd
+import sqlalchemy
+
+get_all_accounts = sqlalchemy.text("""
+SELECT
+"Id",
+"Address"
+FROM public."Accounts"
+ORDER BY "Address"
+LIMIT 100
+""")
 
 def runTask(params):
     print("Running tezos accounts index task")
-    print(connection)
-    
+    print(dbConnection)
+    accounts_df = pd.read_sql(get_all_accounts, dbConnection)
+    print(accounts_df)
